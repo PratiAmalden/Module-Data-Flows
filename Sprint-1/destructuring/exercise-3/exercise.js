a5cb707cde34d514963dadcbfa0964c024f1d25c
shaden-pr <pratiamalden@gmail.com>
2024-12-20 19:28:05 +0000

    write a function to print out the receipt

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,22 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function printReceipt(order) {
+  console.log("QTY     ITEM                TOTAL");
+
+  let totalCost = 0;
+
+  order.forEach(({ itemName, quantity, unitPricePence }) => {
+    const itemTotal = (quantity * unitPricePence) / 100; // convert pence to pounds
+    totalCost += itemTotal;
+
+    console.log(`${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${itemTotal.toFixed(2)}`);
+    // padEnd() adds padding to the end of a string (to align columns)
+  });
+  
+  console.log(`\nTotal:  ${totalCost.toFixed(2)}`);
+
+}
+
+printReceipt(order);
