04d2d51b4b1ab3d85a6be1a4c8ff2c882e9d2238
shaden-pr <pratiamalden@gmail.com>
2024-12-28 20:17:36 +0000

    pass get-average test

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,17 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+    // use Array.prototype.filter to filter out non-numbers
+    const numOnly = numbers.filter(i => typeof i == "number");
+
+    // calculate the sum
+    const sum = numOnly.reduce((acc, num) => acc + num, 0);
+
+    // calculate the average
+    const average = numOnly.length ? sum / numOnly.length : 0;
+
+    return average;
+}
 
 module.exports = average;
