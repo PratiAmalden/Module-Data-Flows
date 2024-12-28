11ab21b64b182c2e750b4f016ce614820d586822
shaden-pr <pratiamalden@gmail.com>
2024-12-28 19:33:56 +0000

    pass car-sales test

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,3 +1,19 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+    // create empty object to store the accumulated sales
+    let totals = {};
+
+    // loop through carSold 
+    // object destructuring for easy access
+    carsSold.forEach(({make, price}) => {
+        if (totals[make]) { // check if make exist in the total obj
+            totals[make] += price; 
+        } else {
+            totals[make] = price;
+        }
+        
+    });
+
+    return totals;
+}
 
 module.exports = sales;
