cffb9f547c60bdb483795ff1d3381e0a0629c1da
shaden-pr <pratiamalden@gmail.com>
2024-12-29 18:44:08 +0000

    write remove vowels test

--- challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.test.js
+++ challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.test.js
@@ -2,8 +2,21 @@ let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
   // Arrange
+  const input = ["Irina", "Etza", "Daniel"];
+  const expected = ["rn", "tz", "Dnl"];
+
   // Act
+  const result = removeVowelsFromWords(
+    input.map((word) =>
+    word.split("")
+    .filter((char) => !"aeiouAEIOU".includes(char))
+    .join("")
+    )
+  );
+
+
   // Assert
+  expect(result).toEqual(expected); // Use toEqual for array comparison
 });
 
 // example
