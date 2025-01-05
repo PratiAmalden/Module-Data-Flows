6ae9fa59a09f2ed86d246ccedee47c627035673d
shaden-pr <pratiamalden@gmail.com>
2025-01-05 21:12:01 +0000

    write convertToOldRoman function with tests

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,36 @@
-function convertToOldRoman(n) {}
+function convertToOldRoman(n) {
+    // Create a mapping of old roman numerals to their values
+    const romanNumeral = [
+        ["M", 1000],
+        ["D", 500],
+        ["C", 100],
+        ["L", 50],
+        ["X", 10],
+        ["V", 5],
+        ["I", 1],
+    ];
+    
+    let result = "";
+
+    // loop through each numeral in romanNumeral
+    for (const [symbol, value] of romanNumeral){
+        // how many times value fits the n
+        const count = Math.floor(n / value);
+
+        // add the numeral "count" times to the result
+        result += symbol.repeat(count);
+
+        // update n to the reminder after using the symbol
+        n %= value;
+
+        /* another way:
+        while (n >= value) {
+            result += symbol;
+            n -= value;
+        } */
+    }
+
+    return result;
+}
 
 module.exports = convertToOldRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -1,7 +1,53 @@
 let convertToOldRoman = require("./convert-to-old-roman");
 
-test("returns I if passed 1 as an argument", function () {
+describe("convertToOldRoman", () => {
+  test("returns I if passed 1 as an argument", function () {
   // Arrange
+  const num = 1;
   // Act
+  const result = convertToOldRoman(num);
   // Assert
-});
+  expect(result).toBe("I");
+  });
+  test("returns V if passed 5 as an argument", function () {
+  // Arrange
+  const num = 5;
+  // Act
+  const result = convertToOldRoman(num);
+  // Assert
+  expect(result).toBe("V");
+  });
+  test("returns III if passed 3 as an argument", function () {
+  // Arrange
+  const num = 3;
+  // Act
+  const result = convertToOldRoman(num);
+  // Assert
+  expect(result).toBe("III");
+  });
+  test("returns VI if passed 6 as an argument", function () {
+  // Arrange
+  const num = 6;
+  // Act
+  const result = convertToOldRoman(num);
+  // Assert
+  expect(result).toBe("VI");
+  });
+  test("returns XXXIII if passed 33 as an argument", function () {
+  // Arrange
+  const num = 33;
+  // Act
+  const result = convertToOldRoman(num);
+  // Assert
+  expect(result).toBe("XXXIII");
+  });
+  test("returns MCXI if passed 1111 as an argument", function () {
+  // Arrange
+  const num = 1111;
+  // Act
+  const result = convertToOldRoman(num);
+  // Assert
+  expect(result).toBe("MCXI");
+  });
+})
+
