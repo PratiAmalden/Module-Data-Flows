e1234688781a757f09d65821a202d6365abf66af
shaden-pr <pratiamalden@gmail.com>
2025-01-05 21:31:05 +0000

    write convertToNewRoman function and tests

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,3 +1,35 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(n) {
+    const romanNumerals = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ];
+
+  let result = "";
+
+  for (const [symbol, value] of romanNumerals) {
+    const count = Math.floor(n / value);
+
+    result += symbol.repeat(count);
+    n %= value;
+  }
+  return result;
+}
+
+console.log(`num = 19: ${convertToNewRoman(19)}`)
+console.log(`num = 44: ${convertToNewRoman(44)}`)
+console.log(`num = 444: ${convertToNewRoman(444)}`)
+
+
 
 module.exports = convertToNewRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -1,7 +1,60 @@
 let convertToNewRoman = require("./convert-to-new-roman");
 
-test("returns I if passed 1 as an argument", function () {
+describe("convertToNewRoman", () => {
+  test("returns I if passed 1 as an argument", function () {
+    // Arrange
+    const num = 1;
+    // Act
+    const result = convertToNewRoman(num);
+    // Assert
+    expect(result).toBe("I")
+  });
+  test("returns IV if passed 4 as an argument", function () {
   // Arrange
+  const num = 4;
   // Act
+  const result = convertToNewRoman(num);
   // Assert
+  expect(result).toBe("IV");
+  });
+  test("returns IX if passed 9 as an argument", function () {
+  // Arrange
+  const num = 9;
+  // Act
+  const result = convertToNewRoman(num);
+  // Assert
+  expect(result).toBe("IX");
+  });
+  test("returns XIV if passed 14 as an argument", function () {
+  // Arrange
+  const num = 14;
+  // Act
+  const result = convertToNewRoman(num);
+  // Assert
+  expect(result).toBe("XIV");
+  });
+  test("returns IX if passed 9 as an argument", function () {
+  // Arrange
+  const num = 9;
+  // Act
+  const result = convertToNewRoman(num);
+  // Assert
+  expect(result).toBe("IX");
+  });
+  test("returns XLIV if passed 44 as an argument", function () {
+  // Arrange
+  const num = 44;
+  // Act
+  const result = convertToNewRoman(num);
+  // Assert
+  expect(result).toBe("XLIV");
+  });
+  test("returns CDXLIV if passed 444 as an argument", function () {
+  // Arrange
+  const num = 444;
+  // Act
+  const result = convertToNewRoman(num);
+  // Assert
+  expect(result).toBe("CDXLIV");
+  });
 });
